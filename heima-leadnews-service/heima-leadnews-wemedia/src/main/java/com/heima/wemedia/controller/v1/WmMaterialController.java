0f63fd88677e0685f5c5df594be328ec5348f7dc
jason <lkarry0199@Gmail.com>
2023-09-03 14:14:50 +0800

    Finish Pic List

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmMaterialController.java
@@ -0,0 +1,26 @@
+package com.heima.wemedia.controller.v1;
+
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmMaterialDto;
+import com.heima.wemedia.service.WmMaterialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1/material")
+public class WmMaterialController {
+
+    @Autowired
+    private WmMaterialService wmMaterialService;
+
+    @PostMapping("/upload_picture")
+    public ResponseResult uploadPicture(@RequestParam("multipartFile") MultipartFile file) {
+        return wmMaterialService.uploadPicture(file);
+    }
+
+    @PostMapping("/list")
+    public ResponseResult findList(@RequestBody WmMaterialDto dto) {
+        return wmMaterialService.findList(dto);
+    }
+}
