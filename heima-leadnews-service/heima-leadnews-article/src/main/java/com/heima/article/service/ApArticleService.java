0b3c0a4fb61200b8b69e78905e98b336072552fe
jason <lkarry0199@Gmail.com>
2023-09-05 10:14:02 +0800

    Finish Article Save

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -1,6 +1,7 @@
 package com.heima.article.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
 import com.heima.model.common.dtos.ResponseResult;
@@ -15,4 +16,11 @@ public interface ApArticleService extends IService<ApArticle> {
      * @return 通用返回类
      */
     public ResponseResult load(ArticleHomeDto dto, Short type);
+
+    /**
+     * 保存app端相关文章
+     * @param dto
+     * @return
+     */
+    public ResponseResult saveArticle(ArticleDto dto);
 }
--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
@@ -1,14 +1,22 @@
 package com.heima.article.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.article.mapper.ApArticleConfigMapper;
+import com.heima.article.mapper.ApArticleContentMapper;
 import com.heima.article.mapper.ApArticleMapper;
 import com.heima.article.service.ApArticleService;
 import com.heima.common.constants.ArticleConstants;
+import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.article.pojos.ApArticleConfig;
+import com.heima.model.article.pojos.ApArticleContent;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -69,5 +77,64 @@ public class ApArticleServiceImpl extends ServiceImpl<ApArticleMapper, ApArticle
 
         // 3. 结果返回
         return ResponseResult.okResult(articleList);
+    }
+
+    @Autowired
+    private ApArticleConfigMapper apArticleConfigMapper;
+
+    @Autowired
+    private ApArticleContentMapper apArticleContentMapper;
+
+    /**
+     * 保存app端相关文章
+     *
+     * @param dto
+     * @return
+     */
+    @Override
+    public ResponseResult saveArticle(ArticleDto dto) {
+        // 1. 检查参数
+        if (dto == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+
+        ApArticle apArticle = new ApArticle();
+        BeanUtils.copyProperties(dto, apArticle);
+
+
+        // 2. 判断是否存在id
+        if (dto.getId() == null) {
+            // 2.1 不存在，保存: 文章 文章配置 文章内容
+
+            //保存文章
+            save(apArticle);
+
+            //保存文章配置
+            ApArticleConfig apArticleConfig = new ApArticleConfig(dto.getId());
+            apArticleConfigMapper.insert(apArticleConfig);
+
+            //保存文章内容
+            ApArticleContent apArticleContent = new ApArticleContent();
+            apArticleContent.setArticleId(apArticle.getId());
+            apArticleContent.setContent(dto.getContent());
+            apArticleContentMapper.insert(apArticleContent);
+
+        } else {
+            // 2.2 存在id，修改: 文章 文章内容 (文章配置是默认的，不需要修改)
+
+            //修改文章
+            updateById(apArticle);
+
+            //修改文章内容
+            ApArticleContent apArticleContent = apArticleContentMapper.selectOne(Wrappers.<ApArticleContent>lambdaQuery().eq(ApArticleContent::getArticleId, apArticle.getId()));
+            if (apArticleContent == null) {
+                return ResponseResult.errorResult(AppHttpCodeEnum.DATA_NOT_EXIST);
+            }
+            apArticleContent.setContent(dto.getContent());
+            apArticleContentMapper.updateById(apArticleContent);
+        }
+
+        // 3. 结果返回，返回文章id
+        return ResponseResult.okResult(apArticle.getId());
     }
 }
